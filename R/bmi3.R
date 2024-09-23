9e558a7c782e0cbd9f6319337a1533bfb515dcdd
kayleeexn <kaylee.t.nguyen-1@ou.edu>
2024-09-23 18:08:06 -0500

    9/23 first commit

---
+++ R/bmi3.R
@@ -0,0 +1,18 @@
+#' Create BMI groups
+#'
+#' More detailed description,
+#'
+#' @param x Numerical vector
+#'
+#' @return Factor variable
+#'
+#' @examples
+#' bmi.vals <- rnorm(n = 20, mean = 25, sd = 3)
+#' bmi3(bmi.vals)
+#'
+#' @export
+
+bmi3 <- function(x) {
+  bmi.groups <- cut (x, breaks = c( 0, 25, 30, Inf), right = FALSE)
+  return (bmi.groups)
+}
---
+++ R/mysquare.R
@@ -0,0 +1,13 @@
+#' The Square Function
+#'
+#' @param x a vector of quantitative data
+#'
+#' @return a vector of quantitative data
+#' @export
+#'
+#' @examples
+#' mysquare(x = 1:10)
+
+mysquare <- function(x){
+  x^2
+}
